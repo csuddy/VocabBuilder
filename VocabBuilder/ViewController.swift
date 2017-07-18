164537af5eb307777150c15e1dafb903117f5573
Work <work@macbookcm.local>
2017-07-18 10:12:03 +0100

    Initial Commit

---
+++ VocabBuilder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VocabBuilder
+//
+//  Created by Work on 18/07/2017.
+//  Copyright © 2017 MurrayIndustries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
